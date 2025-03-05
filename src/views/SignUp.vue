7573b65d1ab4d594521166a13ceef15072fe766d
IvaVozab <ivavozab03@gmail.com>
2025-03-05 09:21:16 +0100

    commit_popravak_logina_05_03_2025_01

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -14,29 +14,28 @@
             <div>
               <label for="username" class="block mb-2 text-sm font-medium text-customGreen dark:text-white">Username</label>
               <input v-model="registerData.username" type="username" name="username" id="username" class="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required>
-              <div v-if="usernameError" class="text-red-500 text-sm mt-2">{{ usernameError }}</div>
+              <div v-if="usernameError" class="text-red-800 text-sm mt-2">{{ usernameError }}</div>
             </div>
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-customGreen dark:text-white">Password</label>
               <input v-model="registerData.password" type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required>
-              <div v-if="passwordError" class="text-red-500 text-sm mt-2">{{ passwordError }}</div>
-              <div v-if="passwordTooShortError" class="text-red-500 text-sm mt-2">{{ passwordTooShortError }}</div>
-              <div v-if="passwordRequiresNonAlphanumericError" class="text-red-500 text-sm mt-2">{{ passwordRequiresNonAlphanumericError }}</div>
-              <div v-if="passwordRequiresDigitError" class="text-red-500 text-sm mt-2">{{ passwordRequiresDigitError }}</div>
-              <div v-if="passwordRequiresUpperError" class="text-red-500 text-sm mt-2">{{ passwordRequiresUpperError }}</div>
-              <div v-if="passwordRequiresLowerError" class="text-red-500 text-sm mt-2">{{ passwordRequiresLowerError }}</div>
+              <div v-if="passwordError" class="text-red-800 text-sm mt-2">{{ passwordError }}</div>
+              <div v-if="passwordTooShortError" class="text-red-800 text-sm mt-2">{{ passwordTooShortError }}</div>
+              <div v-if="passwordRequiresNonAlphanumericError" class="text-red-800 text-sm mt-2">{{ passwordRequiresNonAlphanumericError }}</div>
+              <div v-if="passwordRequiresDigitError" class="text-red-800 text-sm mt-2">{{ passwordRequiresDigitError }}</div>
+              <div v-if="passwordRequiresUpperError" class="text-red-800 text-sm mt-2">{{ passwordRequiresUpperError }}</div>
+              <div v-if="passwordRequiresLowerError" class="text-red-800 text-sm mt-2">{{ passwordRequiresLowerError }}</div>
             </div>
             <div>
               <label for="confirmPassword" class="block mb-2 text-sm font-medium text-customGreen dark:text-white">Confirm Password</label>
               <input v-model="registerData.confirmPassword" type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" class="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required>
-              <div v-if="confirmPasswordError" class="text-red-500 text-sm mt-2">{{ confirmPasswordError }}</div>
+              <div v-if="confirmPasswordError" class="text-red-800 text-sm mt-2">{{ confirmPasswordError }}</div>
             </div>
             <div class="mt-10">
-            <RouterLink @click="register" 
-  :to="'/register/'+(registerData.username || '')" 
+            <button @click="register"  
   class="w-full mt-5 text-white bg-green-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
   Next
-</RouterLink></div>
+</button></div>
             </form>
             <p class="text-sm font-light text-customGreen dark:text-green-700">
            Already have an account? 
@@ -49,9 +48,9 @@
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
 import axios from 'axios';
 
 const router = useRouter();
@@ -88,14 +87,22 @@ const register = async () => {
   username: registerData.value.username,
   password: registerData.value.password
 });
+console.log('Response:', response.status);
+if(response.ok || response.status === 200 ){
+  console.log('Response:', response.data);
+  console.log("Došlo je do funkcije za rutiranje");
+  localStorage.setItem('registeredPassword', registerData.value.password);
 
-console.log('Response:', response.data);
+  router.push(`/register/${registerData.value.username}`);
+}
+else{
+  throw new Error('Registration failed');
+  console.log("Došlo je do greške sa ok response");
 
-// Save password to localStorage
-localStorage.setItem('registeredPassword', registerData.value.password);
+}
+console.log("Preskočlo je else i if za rutiranje");
 
 
-console.log('Response:', response.data);
   } catch (error) {
     console.error('Registration error:', error);
 
